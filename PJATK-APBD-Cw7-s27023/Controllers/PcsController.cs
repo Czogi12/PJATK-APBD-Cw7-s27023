466944a5f88c2723a1a94aa7ad73e2c60fa90ca1
Maciej Nowakowski <s27023@pjwstk.edu.pl>
2026-05-17 18:16:54 +0200

    added templace controller

---
+++ PJATK-APBD-Cw7-s27023/Controllers/PcsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PJATK_APBD_Cw7_s27023.DTOs;
+
+namespace PJATK_APBD_Cw7_s27023.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PcsController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok();
+    }
+    
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetComponentsById([FromRoute] int id)
+    {
+        return Ok();
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Add([FromBody] CreatePcRequest request)
+    {
+        return Ok();
+    }
+    
+    [HttpPut("{id:int}")]
+    public async Task<IActionResult> Update([FromRoute] int id)
+    {
+        return Ok();
+    }
+    
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> Delete([FromRoute] int id)
+    {
+        return Ok();
+    }
+}
