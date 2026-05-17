6bb12bb539bf1d6b2b66c6c45daf391e3cb2ce70
Maciej Nowakowski <s27023@pjwstk.edu.pl>
2026-05-17 19:03:20 +0200

    updated interface

--- PJATK-APBD-Cw7-s27023/Services/IPcService.cs
+++ PJATK-APBD-Cw7-s27023/Services/IPcService.cs
@@ -6,5 +6,8 @@ namespace PJATK_APBD_Cw7_s27023.Services;
 public interface IPcService
 {
     public Task<IEnumerable<PcResponse>> GetAll(CancellationToken token);
-    public Task<IEnumerable<PCComponent>> GetAllComponents(int pcId, CancellationToken token);
+    public Task<PcWithComponentsResponse?> GetAllComponents(int pcId, CancellationToken token);
+    Task<PcResponse> Create(CreatePcRequest request, CancellationToken token);
+    Task<bool> Update(int pcId, UpdatePcRequest request, CancellationToken token);
+    Task<bool> Delete(int pcId, CancellationToken token);
 }
