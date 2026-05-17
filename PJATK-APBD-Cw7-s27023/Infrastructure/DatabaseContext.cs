a2fceb784370f85879797813257dd42b21307ea7
Maciej Nowakowski <s27023@pjwstk.edu.pl>
2026-05-17 16:43:21 +0200

    added DbSets

--- PJATK-APBD-Cw7-s27023/Infrastructure/DatabaseContext.cs
+++ PJATK-APBD-Cw7-s27023/Infrastructure/DatabaseContext.cs
@@ -5,6 +5,12 @@ namespace PJATK_APBD_Cw7_s27023.Infrastructure;
 
 public class DatabaseContext(DbContextOptions opt) : DbContext(opt)
 {
+    public DbSet<Component> Components { get; set; }
+    public DbSet<PCComponent> PCComponents { get; set; }
+    public DbSet<PC> PCs { get; set; }
+    public DbSet<ComponentManufacturer> ComponentManufacturers { get; set; }
+    public DbSet<ComponentType> ComponentTypes { get; set; }
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
