700bafb7d195d0dc46d0570a568c61db5484e667
Maciej Nowakowski <s27023@pjwstk.edu.pl>
2026-05-17 18:17:54 +0200

    added get and post

---
+++ PJATK-APBD-Cw7-s27023/DTOs/PcResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace PJATK_APBD_Cw7_s27023.DTOs;
+
+public record PcResponse(
+    int Id,
+    string Name,
+    double Weight,
+    int Warranty,
+    DateTime CreatedAt,
+    int Stock
+    );
---
+++ PJATK-APBD-Cw7-s27023/Services/IPcService.cs
@@ -0,0 +1,11 @@
+﻿using PJATK_APBD_Cw7_s27023.DTOs;
+using PJATK_APBD_Cw7_s27023.Models;
+
+namespace PJATK_APBD_Cw7_s27023.Services;
+
+public interface IPcService
+{
+    public Task<IEnumerable<PcResponse>> GetAll(CancellationToken token);
+    public Task<IEnumerable<PCComponent>> GetAllComponents(int pcId,CancellationToken token);
+    
+}
