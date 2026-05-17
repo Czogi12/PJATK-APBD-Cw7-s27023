5b4cd87680ad4565baf75da57aa4229edc71c20e
Maciej Nowakowski <s27023@pjwstk.edu.pl>
2026-05-18 00:36:41 +0200

    updated controller

--- PJATK-APBD-Cw7-s27023/Controllers/PcsController.cs
+++ PJATK-APBD-Cw7-s27023/Controllers/PcsController.cs
@@ -1,39 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PJATK_APBD_Cw7_s27023.DTOs;
+using PJATK_APBD_Cw7_s27023.Services;
 
 namespace PJATK_APBD_Cw7_s27023.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class PcsController : ControllerBase
+public class PcsController(IPcService pcService) : ControllerBase
 {
     [HttpGet]
-    public async Task<IActionResult> GetAll()
+    public async Task<IActionResult> GetAll(CancellationToken token)
     {
-        return Ok();
+        var pcs = await pcService.GetAll(token);
+        return Ok(pcs);
     }
     
     [HttpGet("{id:int}")]
-    public async Task<IActionResult> GetComponentsById([FromRoute] int id)
+    public async Task<IActionResult> GetComponentsById([FromRoute] int id, CancellationToken token)
     {
-        return Ok();
+        var pc = await pcService.GetAllComponents(id, token);
+        return pc is null ? NotFound() : Ok(pc);
     }
     
     [HttpPost]
-    public async Task<IActionResult> Add([FromBody] CreatePcRequest request)
+    public async Task<IActionResult> Add([FromBody] CreatePcRequest request, CancellationToken token)
     {
-        return Ok();
+        var pc = await pcService.Create(request, token);
+        return CreatedAtAction(nameof(GetComponentsById), new { id = pc.Id }, pc);
     }
     
     [HttpPut("{id:int}")]
-    public async Task<IActionResult> Update([FromRoute] int id)
+    public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdatePcRequest request, CancellationToken token)
     {
-        return Ok();
+        var updated = await pcService.Update(id, request, token);
+        return updated ? NoContent() : NotFound();
     }
     
     [HttpDelete("{id:int}")]
-    public async Task<IActionResult> Delete([FromRoute] int id)
+    public async Task<IActionResult> Delete([FromRoute] int id, CancellationToken token)
     {
-        return Ok();
+        var deleted = await pcService.Delete(id, token);
+        return deleted ? NoContent() : NotFound();
     }
 }
